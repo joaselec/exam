54bebf0061998d109679e3fa6eadd41d3329da03
kim <kim@mail.com>
2019-12-06 10:16:03 +0900

    191206

--- stock/cron.py
+++ stock/cron.py
@@ -5,6 +5,18 @@ def my_scheduled_job():
     #CronLog.save()
     print("cron")
 
+a = 0
+
+def fileRW():
+    try:
+        global a
+        f = open("test.txt", 'a')
+        f.write(str(a))
+        a = a + 1
+    finally:
+        f.close()
+
+
 def my_cron_job():
     #pass
     print("cron")
